c3662d7a33ffa418017281a239957a0e1f1db71a
LOGESH0603 <logeshibl@gmai.com>
2025-08-10 01:12:14 +0530

    ui/ux updation

--- my-excel-analytics-app/src/App.js
+++ my-excel-analytics-app/src/App.js
@@ -1,47 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import LoginPage from './LoginPage';
-import RegistrationPage from './RegistrationPage';
-import SuperAdminDashboard from './SuperAdminDashboard';
-import AdminDashboard from './AdminDashboard';
-import UserDashboard from './UserDashboard';
-import AboutUsPage from './AboutUsPage';
-import ContactUsPage from './ContactUsPage';
-import CareersPage from './CareersPage';
-import BlogPage from './BlogPage';
-import PrivacyPolicyPage from './PrivacyPolicyPage';
-import TermsOfServicePage from './TermsOfServicePage';
-import DisclaimerPage from './DisclaimerPage';
-import FeaturesPage from './FeaturesPage';
-import PricingPage from './PricingPage';
-import PrivateRoute from './PrivateRoute';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import LoginPage from "./LoginPage";
+import RegistrationPage from "./RegistrationPage";
+import SuperAdminDashboard from "./SuperAdminDashboard";
+import AdminDashboard from "./AdminDashboard";
+import UserDashboard from "./UserDashboard";
+import AboutUsPage from "./AboutUsPage";
+import ContactUsPage from "./ContactUsPage";
+import CareersPage from "./CareersPage";
+import BlogPage from "./BlogPage";
+import PrivacyPolicyPage from "./PrivacyPolicyPage";
+import TermsOfServicePage from "./TermsOfServicePage";
+import DisclaimerPage from "./DisclaimerPage";
+import FeaturesPage from "./FeaturesPage";
+import PricingPage from "./PricingPage";
+import PrivateRoute from "./PrivateRoute";
+import Footer from "./Footer"; // Import the footer component
 
 const App = () => {
   return (
     <Router>
-      <div className="bg-gray-100 min-h-screen">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
+      <div className="bg-gray-100 min-h-screen flex flex-col">
+        {/* Main content area */}
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
 
-          <Route path='/' element={<PrivateRoute />}>
-          <Route path="/super-admin" element={<SuperAdminDashboard />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route path="/user-dashboard" element={<UserDashboard />} />
-          </Route>
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="/super-admin" element={<SuperAdminDashboard />} />
+              <Route path="/admin-dashboard" element={<AdminDashboard />} />
+              <Route path="/user-dashboard" element={<UserDashboard />} />
+            </Route>
 
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/contact-us" element={<ContactUsPage />} />
-          <Route path="/careers" element={<CareersPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="/terms-of-service" element={<TermsOfServicePage />} />
-          <Route path="/disclaimer" element={<DisclaimerPage />} />
-          <Route path="/features" element={<FeaturesPage />} />
-          <Route path="/pricing" element={<PricingPage />} />
-        </Routes>
+            <Route path="/about-us" element={<AboutUsPage />} />
+            <Route path="/contact-us" element={<ContactUsPage />} />
+            <Route path="/careers" element={<CareersPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+            <Route path="/disclaimer" element={<DisclaimerPage />} />
+            <Route path="/features" element={<FeaturesPage />} />
+            <Route path="/pricing" element={<PricingPage />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
