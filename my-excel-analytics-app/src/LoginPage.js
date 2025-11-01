4e2ac056cfa69b841de8abe9b06bbaef1dd121f0
RAJNEESH <imrajneeshverma@gmail.com>
2025-11-01 12:11:10 +0530

    LoginPage Update

--- my-excel-analytics-app/src/LoginPage.js
+++ my-excel-analytics-app/src/LoginPage.js
@@ -90,7 +90,7 @@ const LoginPage = () => {
     e.preventDefault();
     setError('');
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
